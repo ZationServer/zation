568d6e7a8d0b6404a95c327e567c544af9cf19bc
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2018-04-01 23:03:23 +0200

    update options

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const zationServer = require('zation-server');
-module.exports = (options) => {
-    return zationServer(options);
+module.exports = (options1,options2) => {
+    return zationServer(options1,options2);
 };
