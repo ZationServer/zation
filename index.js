b926617e083454e302160327c06e737675ef2680
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2018-04-02 00:02:16 +0200

    add api support

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const zationServer = require('zation-server');
-module.exports = (options1,options2) => {
-    return zationServer(options1,options2);
+
+module.exports.start = (options1,options2) =>
+{
+    zationServer.start(options1,options2);
 };
+
+//Api Classes
+module.exports.Bag = zationServer.Bag;
+module.exports.Controller = zationServer.Controller;
+module.exports.Result = zationServer.Result;
+module.exports.TaskError = zationServer.TaskError;
+module.exports.TaskErrorBag = zationServer.TaskErrorBag;
