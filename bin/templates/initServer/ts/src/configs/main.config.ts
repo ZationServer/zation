1bfb4901ef4826dde13f389d210eb06d0dbe15a4
Luca Scaringella <lucalstyle@gmail.com>
2019-02-09 20:31:19 +0100

    fix username panel bug, update to version 0.6.4

--- bin/templates/initServer/ts/src/configs/main.config.ts
+++ bin/templates/initServer/ts/src/configs/main.config.ts
@@ -5,5 +5,5 @@ module.exports = Config.mainConfig(
         port: {{port}},
         appName : '{{appName}}',{{timeZone}}
         usePanel : true,
-        panelUser : {userName: '{{panelUserName}}', password: '{{panelPassword}}'},
+        panelUser : {username: '{{panelUserName}}', password: '{{panelPassword}}'},
     });
