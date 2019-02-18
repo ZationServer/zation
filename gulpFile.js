79208c31e76c6fb340241196e70f73932a96efea
Luca Scaringella <lucalstyle@gmail.com>
2019-02-18 12:03:58 +0100

    remove scss compile, update to version 0.6.5

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,5 @@
 const gulp       = require('gulp');
 const typescript = require('gulp-typescript');
-const sass       = require('gulp-sass');
 const tscConfig  = require('./tsconfig.json');
 
 gulp.task('ts', function () {
@@ -10,24 +9,17 @@ gulp.task('ts', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('scss', function() {
-    return gulp.src('src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist'));
-});
-
 gulp.task('cof', function() {
     return gulp
-        .src(['src/**/*','!src/**/*.ts','!src/**/*.scss'])
+        .src(['src/**/*','!src/**/*.ts'])
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('compile', gulp.parallel('scss','cof','ts'));
+gulp.task('compile', gulp.parallel('cof','ts'));
 
 gulp.task('watch', function() {
     gulp.watch('src/**/*.ts', gulp.parallel('ts'));
-    gulp.watch('src/**/*.scss', gulp.parallel('scss'));
-    gulp.watch(['src/**/*','!src/**/*.ts','!src/**/*.scss'], gulp.parallel('cof'));
+    gulp.watch(['src/**/*','!src/**/*.ts'], gulp.parallel('cof'));
 });
 
 gulp.task('default', gulp.series('compile','watch'));
