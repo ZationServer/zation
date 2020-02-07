ab12031ab9c62d7c4fb43ae27a01b4ef2b7c54b5
Luca Scaringella <lucalstyle@gmail.com>
2020-02-07 03:48:11 +0100

    Update zation versions for bundle version 0.0.1

--- src/versions.ts
+++ src/versions.ts
@@ -1,9 +1,9 @@
-export const zationBundleVersion = '0.0.0';
-export const zationServerVersion = '1.4.2';
-export const zationClientVersion = '1.0.1';
-export const zationAssuredVersion = '0.3.5';
-export const gulpTypescriptVersion = '5.0.0';
-export const typescriptVersion = '3.5.3';
+export const zationBundleVersion = '0.0.1';
+export const zationServerVersion = '1.4.7';
+export const zationClientVersion = '1.0.2';
+export const zationAssuredVersion = '0.3.6';
+export const gulpTypescriptVersion = '^5.0.1';
+export const typescriptVersion = '^3.7.5';
 export const zationClusterStateVersion = '6.0.6';
 export const zationClusterBrokerVersion = '6.0.7';
 
