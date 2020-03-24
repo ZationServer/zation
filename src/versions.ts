c2b18e038e8e3102b4d606a6fc1432c061d9e21a
Luca Scaringella <lucalstyle@gmail.com>
2020-03-24 01:22:21 +0100

    Add more version variables.

--- src/versions.ts
+++ src/versions.ts
@@ -2,18 +2,35 @@ export const zationBundleVersion = '0.0.5';
 export const zationServerVersion = '1.5.4';
 export const zationClientVersion = '1.1.4';
 export const zationAssuredVersion = '0.4.0';
-export const gulpTypescriptVersion = '^5.0.1';
-export const typescriptVersion = '^3.8.2';
 export const zationClusterStateVersion = '6.0.6';
 export const zationClusterBrokerVersion = '6.0.7';
 
-export const versions = {
+export const zationBundle = {
     zationBundleVersion,
     zationServerVersion,
     zationClientVersion,
     zationAssuredVersion,
-    gulpTypescriptVersion,
-    typescriptVersion,
     zationClusterStateVersion,
-    zationClusterBrokerVersion
+    zationClusterBrokerVersion,
+};
+
+export const gulpVersion = '4.0.2';
+export const gulpTypescriptVersion = '^5.0.1';
+export const gulpSourcemapsVersion = '^2.6.5';
+export const typescriptVersion = '^3.8.2';
+export const delVersion = '5.1.0';
+export const mochaVersion = '^6.1.4';
+export const tsNodeVersion = '^8.2.0';
+export const typesNodeVersion = '^13.7.6';
+
+export const versions = {
+    ...zationBundle,
+    gulpVersion,
+    gulpTypescriptVersion,
+    gulpSourcemapsVersion,
+    typescriptVersion,
+    delVersion,
+    mochaVersion,
+    tsNodeVersion,
+    typesNodeVersion
 };
