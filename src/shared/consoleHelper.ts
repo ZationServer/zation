c9ee2bb08d077b9853ef51b59d8c66982470fe52
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-06-27 00:08:58 +0200

    Remove show npm commands command.

--- src/shared/consoleHelper.ts
+++ src/shared/consoleHelper.ts
@@ -44,27 +44,5 @@ export const print = {
     welcome: () => {
         term.cyan(`Thank you for using the Zation Framework 🚀 (Bundle:${zationBundleVersion}) (https://github.com/ZationServer)!\n`);
     },
-    projectNpmCommands: () => {
-        console.log();
-        console.log('Npm project commands (Only works in Zation CLI created projects):');
-        console.log('  For server project:');
-        console.log(`     npm start                           Starts the server.`);
-        console.log(`     npm test                            Runs all tests.`);
-        console.log(`     npm run check                       Checks all Zation configurations.`);
-        console.log(`     npm run build                       Builds the project.`);
-        console.log(`     npm run watch                       Starts to build automatically when the source code changes.`);
-        console.log(`     npm run dockerBuild                 Builds an docker image.`);
-        console.log(`     npm run dockerExport                Exports the docker image in the project folder.`);
-        console.log(`     npm run dockerRun                   Runs docker container.`);
-        console.log(`     npm run dockerStop                  Stops docker container.`);
-        console.log('  For client project:');
-        console.log('    Web client:');
-        console.log(`       npm start                         Starts the client in the browser and build it with webpack.`);
-        console.log(`       npm run build                     Builds the client with webpack.`);
-        console.log('    Node client:');
-        console.log(`       npm start                         Starts the client and build it.`);
-        console.log(`       npm run build                     Builds the client.`);
-        console.log(`       npm run watch                     Starts to build automatically when a file changes.`);
-    }
 };
 
